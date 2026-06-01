d26a00266f94a3275d76e80fbc3149d6e73806d6
Eduard Gilmutdinov <edgilmutdinov@gmail.com>
2026-06-01 07:15:20 +0500

    Clean up

--- macos/set_links.sh
+++ macos/set_links.sh
@@ -6,6 +6,5 @@ ln -s $PWD/zprofile ~/.zprofile
 ln -s $PWD/tmux.conf ~/.config/tmux/tmux.conf
 ln -s $PWD/tmux.tokyonight.conf ~/.config/tmux/tmux.tokyonight.conf
 ln -s $PWD/gitconfig ~/.gitconfig
-ln -s $PWD/nnn_preview_cmd.sh ~/nnn_preview_cmd.sh
 ln -s $PWD/bat ~/.config/bat
 ln -s $PWD/lazygit.yml ~/Library/"Application Support"/lazygit/config.yml
